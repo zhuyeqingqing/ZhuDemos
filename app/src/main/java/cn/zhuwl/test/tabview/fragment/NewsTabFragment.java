d420e5360488b0e7f9d2515308fe957656de4885
zhuwenliang <zhuwenliang@ushareit.com>
2021-10-09 19:26:40 +0800

    闪动文字

--- app/src/main/java/cn/zhuwl/test/tabview/fragment/NewsTabFragment.java
+++ app/src/main/java/cn/zhuwl/test/tabview/fragment/NewsTabFragment.java
@@ -1,8 +1,8 @@
 package cn.zhuwl.test.tabview.fragment;
 
-import android.annotation.SuppressLint;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +12,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import cn.zhuwl.test.R;
 
@@ -34,8 +33,19 @@ public class NewsTabFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tab_news, container, false);
         TextView tv_Name = rootView.findViewById(R.id.tv_news);
-        //Todo
+        TextView tv_news_shan = rootView.findViewById(R.id.tv_news_shan);
+        int[] colors = new int[]{getResources().getColor(R.color.tab_text_start_color,null),
+                getResources().getColor(R.color.tab_text_end_color,null),
+                getResources().getColor(R.color.tab_indicator_color,null)};
+
         tv_Name.setText(mNewsClassifyName);
+        tv_news_shan.setText(mNewsClassifyName);
+        LinearGradient linearGradient = new LinearGradient(0f,0f,100,0f,colors,new float[]{0f,0.5f, 1f},
+                Shader.TileMode.CLAMP);
+        tv_Name.setTextSize(18);
+        tv_Name.getPaint().setShader(linearGradient);
+        tv_Name.requestLayout();
+        tv_Name.invalidate();
         return rootView;
 
     }
